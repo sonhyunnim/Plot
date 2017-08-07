32f91c622008f1fe80436a438b800f1d739f9acc
2sjeong918 <2sjeong918@gmail.com>
2017-08-07 17:46:27 +0900

    개발환경 세팅, 컴포넌트 분류, 라우팅 연결

---
+++ src/components/Main/BannerCarousel.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="main-banner">
+    <div class="music-player">
+      <ul class="music-coverlist is-list">
+        <li class="music-cover-item main-banner">
+            <img class="banner music-cover is-rwd" src="../../assets/images/main_banner01.png" >
+        </li>
+      </ul>
+      <div class="player-indicators">
+        <a href role="tab" class="indicator is-active" aria-label=""></a>
+        <a href role="tab" class="indicator" aria-label=""></a>
+        <a href role="tab" class="indicator" aria-label=""></a>
+        <a href role="tab" class="indicator" aria-label=""></a>
+        <a href role="tab" class="indicator" aria-label=""></a>
+      </div>
+      <div class="player-controller">
+        <button type="button" class="button is-button is-prev" aria-label="PREV"></button>
+        <button type="button" class="button is-button is-next" aria-label="NEXT"></button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+     return {
+     coverList : [
+  {
+    src: "001.EdSheeran-ShapeOfYou.jpg",
+    alt: "Ed Sheeran - Shape Of You"
+  },
+  {
+    src: "002.TheChainsmokers&Halsey-Closer.jpg",
+    alt: "The Chainsmokers & Halsey - Closer"
+  },
+  {
+    src: "003.Zayn&TaylorSwift-IDon'tWannaLiveForever(FiftyShadesDarker).jpg",
+    alt: "Zayn & TaylorSwift - I Don't Wanna Live Forever (Fifty Shades Darker)"
+  },
+  {
+    src: "004.KatyPerry&SkipMarley-ChainedToTheRhythm.jpg",
+    alt: "Katy Perry & Skip Marley - Chained To The Rhythm"
+  },
+  {
+    src: "005.Migos&LilUziVert-BadAndBoujee.jpg",
+    alt: "Migos & Lil Uzi Vert - Bad And Boujee"
+  },
+  {
+    src: "006.TheChainsmokers-Paris.jpg",
+    alt: "The Chainsmokers - Paris"
+  },
+  {
+    src: "007.Rihanna-LoveOnTheBrain.jpg",
+    alt: "Rihanna - Love On The Brain"
+  },
+  {
+    src: "008.BrunoMars-That'sWhatILike.jpg",
+    alt: "Bruno Mars - That's What I Like"
+  },
+  {
+    src: "009.BigSean-BounceBack.jpg",
+    alt: "Big Sean - Bounce Back"
+  },
+  {
+    src: "010.MachineGunKelly&CamilaCabello-BadThings.jpg",
+    alt: "Machine Gun Kelly & Camila Cabello - Bad Things"
+  }
+]
+
+    }
+  },
+  
+}
+</script>
+<style lang="sass" scoped>
+  @import "~config"
+  .main-banner
+    background: black
+    width: 100%
+    
+  .is-list 
+    margin-top: 0
+    margin-bottom: 0
+    padding-left: 0
+    list-style: none
+  .is-rwd 
+    width: 100%
+    height: auto
+  .is-button 
+    border: none
+    background: none
+  .music-player 
+    position: relative
+    overflow: hidden
+    padding-top: 35%
+    
+  .banner
+    background-size: cover
+  .music-cover
+    position: absolute
+    width: 80%
+    height: auto
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+  .music-coverlist 
+    // display: flex
+    // transform: translateX(0px)
+    // transition: all 0.4s cubic-bezier(0.92, 0, 0.49, 0.47)
+  .music-cover-item 
+    flex: 1 0 400px
+  .player-controller 
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+  .player-controller .button 
+    opacity: 0.85
+    position: absolute
+    top: 50%
+    width: 24px
+    height: 40px
+    // background: url("../images/icon-arrow-prev-next-bw.png") no-repeat
+    background-size: 48px 80px
+    transform: translateY(-50%)
+    transition: all 0.4s
+  .player-controller .is-prev 
+    left: 10px
+    background-position: 0 100%
+
+  .player-controller .is-next 
+    right: 10px
+    background-position: 100% 100%
+
+  .player-indicators 
+    position: absolute
+    z-index: 100
+    bottom: 20px
+    left: 50%
+    transform: translateX(-50%)
+  .indicator.is-active 
+    opacity: 1
+    background: rgb(255, 255, 255)
+    transform: scale(1.6)
+  .indicator 
+    opacity: 1
+    cursor: pointer
+    display: inline-block
+    width: 10px
+    height: 10px
+    background: #fff
+    border-radius: 50%
+    margin-left: 5px
+    margin-right: 5px
+    font-size: 14px
+    transition: all .3s ease
+   
+
+</style>
---
+++ src/components/Main/Category.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="category container">
+    <h2 class="cg-heading container">카테고리</h2>
+    <ul class="container">
+      <li class="cg-list"><a href="">장르별</a></li>
+      <li class="cg-list"><a href="">지역별</a></li>
+      <li class="cg-list"><a href="">날짜별</a></li>
+      <li class="cg-sort"><a href="">추천순</a></li>
+    </ul>
+    <ul class="cg-items container">
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster01.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster02.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster03.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster04.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster02.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster01.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster02.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster03.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster04.gif" alt=""></a>
+      </router-link>
+      <router-link to="/detail"  class="active" active-class="activated"  tag="li"> 
+          <a href><img src="../../assets/images/poster02.gif" alt=""></a>
+      </router-link>
+    </ul>
+    <div class="more"><a href="">더보기</a></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+     return {
+
+
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+  @import "~config"
+  
+  .container
+    +container(1190px 12)
+    // +global-box-sizing(border-box)
+
+  .category
+    // background: blue
+    margin-top: leading(2)
+  .cg-heading
+    background: #1e0b65
+    color: #fff
+    font: bold 2rem "Noto Sans kr", sans-serif
+    +span(2)
+    line-height: leading(2)
+    height: leading(2)
+    margin-right: span(10)
+  .cg-list, .more
+    a
+      +span(2)
+      line-height: leading(2)
+      color: #1e0b65
+      border: 2px solid #1e0b65
+      box-sizing: border-box
+      height: leading(2)
+      margin-top: leading(2)
+      text-decoration: none
+      font: bold 2rem "Noto Sans kr", sans-serif
+  .more a
+    +push(5)
+  .cg-sort
+    a
+      +span(2 last)
+      line-height: leading(2)
+      color: #1e0b65
+      border: 2px solid #1e0b65
+      height: leading(2)
+      margin-top: leading(2)
+      text-decoration: none
+      font: bold 2rem "Noto Sans kr", sans-serif
+  .cg-items
+    +container(10 nest)
+    li
+      +span(2 of 10 .15)
+      // height: auto
+      background: #1e0b65
+      // border: 2px solid #fff
+      margin-top: leading(2)
+      position: relative
+      overflow: hidden
+      padding-top: 30%
+      img
+        position: absolute
+        width: auto
+        height: 85%
+        top: 0
+        left: 50%
+        transform: translateX(-50%)
+
+</style>
