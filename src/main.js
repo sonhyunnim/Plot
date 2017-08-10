d9e6eb17741b4c25361e8c5d0c51ce41d474752a
2sjeong918 <2sjeong918@gmail.com>
2017-08-10 14:45:58 +0900

    카테고리 리스트 토글 작업중

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './App';
 import toggle from './toggle';
 import router from './router';
+import axios from 'axios';
 
+Vue.prototype.$http = axios;
 
 new Vue({
   el: '#app',
