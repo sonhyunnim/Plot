8eeafa69d8397be0ea93b6ac90aa59b8a3879d5c
Dain_Kang <Dain_Kang@Dain-Kangui-MacBook-Pro.local>
2017-08-21 21:06:26 +0900

    랭킹 부분수정

--- src/components/Main/RankingItem.vue
+++ src/components/Main/RankingItem.vue
@@ -1,6 +1,7 @@
 <template>
-  <transition name="fade">
-    <div class="carousel-item" v-show="is_visible">
+  <transition name="slide">
+    <!-- <div class="ranking-item" v-show="is_visible"> -->
+    <div class="ranking-item">
      <slot></slot>
     </div>
   </transition>
@@ -8,23 +9,33 @@
 <script>
 
 export default {
-  name: 'CarouselItem',
+  name: 'RankingItem',
   props:{
     index: {
       type: Number,
       default : 0
+    },
+    active_index: {
+      type: Number,
+      default: 0
     }
   },
   data(){
     return {
        //index 초기값 설정.
       // index: 0
+      // active_index: this.index,
+
     }
   },
   computed: {
     is_visible(){
       // console.log('this.$parent.active_index:',this.$parent.active_index)
       // console.log('this.index:',this.index)
+      //리스트의 현제 인덱스(this.index) ===보여지는 (active_index)
+      // console.log('this.$parent',this.$parent)
+      // console.log('this.index',this.index)
+      // console.log('this.active_index',this.active_index)
       return this.$parent.active_index === this.index;
     },
     
@@ -35,17 +46,17 @@ export default {
 </script>
 <style lang="sass" scoped>
   @import "~config"
-  .carousel-item
+  .ranking-item
     position: absolute
     left: 0
     right: 0
-    top: 0
+    top: leading(1)
     bottom: 0
-  .fade-enter,
-  .fade-leave-to
+  .slide-enter,
+  .slide-leave-to
     opacity: 0
-  .fade-enter-active,
-  .fade-leave-active
+  .slide-enter-active,
+  .slide-leave-active
     transition: opacity 2s 
     
     
