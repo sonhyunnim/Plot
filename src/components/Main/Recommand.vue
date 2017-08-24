23ed96429462f590133d2baa9f950cc67735d311
hyun <hyun@smgui-MacBook-Pro.local>
2017-08-24 14:28:18 +0900

    추천전시 부분 설계 및 스타일링 수정

--- src/components/Main/Recommand.vue
+++ src/components/Main/Recommand.vue
@@ -1,20 +1,53 @@
 <!--Recommand Carousel --> 
 <template>
   <div id="recommand" class="recommand">
-    <ul class="rc-items container">
-      <li class="rc-item1">item1</li>
-      <li class="rc-item2">item2</li>
-      <li class="rc-item3">item3</li>
-      <li class="rc-item4">item4</li>
-    </ul>
+    <h2 class="re-heading">추천 전시</h2>
+        <div class="slides-view" :style="{width: 260 * number +'px'}">
+          <div class="slides-inner" :style="{width: 260 * slides.length +'px'}">
+            <div class="img-wrapper" v-for="(slide,index) in slides"
+              :key="index"> 
+              <recommand-slide :slide="slide"></recommand-slide>
+            </div>
+          </div>
+        </div>
+        <div class="arrow_btn">
+          <button class="slide_btn is_prev" type="button" @click="preve">
+            <i class="fa fa-chevron-left" aria-hidden="true"alt="prev item"></i>
+          </button>
+          <button class="slide_btn is_next" type="button" @click="next">
+            <i class="fa fa-chevron-right" aria-hidden="true"alt="next item"></i>
+          </button>
+        </div>
   </div>
 </template>
 <script>
+import RecommandSlide from './RecommandSlide';
+
 export default {
+  components: {
+    RecommandSlide
+  },
   data() {
-     return {
-
-
+    return {
+      slides: [
+        { src: 'img01.png', alt: "alt 01"},
+        { src: 'img02.png', alt: "alt 02"},
+        { src: 'img03.png', alt: "alt 03"},
+        { src: 'img04.png', alt: "alt 04"},
+        { src: 'img05.png', alt: "alt 05"},
+        { src: 'img06.png', alt: "alt 06"},
+        { src: 'img07.png', alt: "alt 07"},
+        { src: 'img08.png', alt: "alt 08"}
+      ],
+      number: 4
+    }  
+  },
+  methods: {
+    preve: function () {
+      return this.number -= 4;
+    },
+    next: function () {
+      return this.number += 4;
     }
   }
 }
@@ -24,11 +57,55 @@ export default {
   
   .container
     +container(1190px 12)
-  .recommand
-    margin-top: leading(2)
-  .rc-items li
-    +span(3)
-    height: leading(10)
+
+  .re-heading
+    +span(2)
+    margin-top: leading(-3)
+    padding-left: leading(1)/2
     background: #1e0b65
+    color: #fff
+    font: bold 2rem "Noto Sans kr", sans-serif
+    line-height: leading(2)
+    height: leading(2)
+    margin-right: span(10)
+
+  .recommand
+    margin-top: leading(4)
+    background-color: #1e0b65
+    position: relative
+    height: leading(10)
+
+  .slides-view
+    width: 260px
+    margin: 0 auto
+    overflow: hidden
+    height: 230px
+   
+  .img-wrapper
+    float: left
+  .slides-inner
+    height: 230px
+    margin-top: span(0.4)
+
+  button
+    width: 3.5rem
+    height: 3.5rem
+  
+  .slide_btn
+    position: absolute
+    bottom: 40%
+    border: none
+    opacity: 1
+    background-color: #1e0b65
+    color: #fff
+    transition: opacity 0.4s
+    transform: translateY(-50%)
+    &.is_prev
+      left: 40px
+    &.is_next
+      right: 40px  
+    &:hover
+      color: rgba(255, 255, 255, 0.5)
+
     
 </style>
---
+++ src/components/Main/RecommandSlide.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="slide">
+    <!-- hello slide? {{ slide.src }} -->
+    <img :src="proofingSlide" alt="slide.alt">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  // 오류 발생원인 : props 문법 에러 문제 
+  props: ['slide'],
+  computed: {
+    proofingSlide(){
+      return require(`../../assets/images/${this.slide.src}`);
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import "~config"
+  img
+    width: 260px
+    height: 200px
+    // padding-left: span(1)
+  
+  .slide
+    // background-color: #1e0b65
+ 
+</style>
